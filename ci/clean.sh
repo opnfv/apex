60a044c54bd1bc030d3fee675e05f5879e5ce636
Tim Rozet <trozet@redhat.com>
2015-10-28 15:17:20 -0400

    Adds HA support to deploy
    Defaults the deploy to use HA and adds new param for using
    non-HA.
    
    JIRA: APEX-40
    
    Change-Id: I33059c7094f3d08d793974b0482e260ac954bc63
    
    Signed-off-by: Tim Rozet <trozet@redhat.com>

--- ci/clean.sh
+++ ci/clean.sh
@@ -5,13 +5,13 @@
 #
 #Uses Vagrant and VirtualBox
 #
+vm_index=4
 virsh destroy instack 2> /dev/null || echo -n ''
 virsh undefine instack --remove-all-storage 2> /dev/null || echo -n ''
-virsh destroy baremetalbrbm_0 2> /dev/null || echo -n ''
-virsh undefine baremetalbrbm_0 --remove-all-storage 2> /dev/null || echo -n ''
-virsh destroy baremetalbrbm_1 2> /dev/null || echo -n ''
-virsh undefine baremetalbrbm_1 --remove-all-storage 2> /dev/null || echo -n ''
 
 rm -f /var/lib/libvirt/images/instack.qcow2 2> /dev/null
-rm -f /var/lib/libvirt/images/baremetalbrbm_0.qcow2 2> /dev/null
-rm -f /var/lib/libvirt/images/baremetalbrbm_1.qcow2 2> /dev/null
+for i in $(seq 0 vm_index); do
+  virsh destroy baremetalbrbm_$i 2> /dev/null || echo -n ''
+  virsh undefine baremetalbrbm_$i --remove-all-storage 2> /dev/null || echo -n ''
+  rm -f /var/lib/libvirt/images/baremetalbrbm_${i}.qcow2 2> /dev/null
+done
