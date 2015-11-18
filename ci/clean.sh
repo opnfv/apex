44cf1189df87fac5703e5bab664a166ad91304ce
Dan Radez <dradez@redhat.com>
2015-11-17 19:13:37 -0500

    adding vol-delete to cleanup.sh
    Change-Id: I3cbb79b09c6ac77dbc3c452938dbd6a6d03c815b

--- ci/clean.sh
+++ ci/clean.sh
@@ -8,10 +8,13 @@
 vm_index=4
 virsh destroy instack 2> /dev/null || echo -n ''
 virsh undefine instack --remove-all-storage 2> /dev/null || echo -n ''
-
+virsh vol-delete instack.qcow2 default 2> /dev/null
 rm -f /var/lib/libvirt/images/instack.qcow2 2> /dev/null
+
 for i in $(seq 0 $vm_index); do
   virsh destroy baremetalbrbm_brbm1_$i 2> /dev/null || echo -n ''
   virsh undefine baremetalbrbm_brbm1_$i --remove-all-storage 2> /dev/null || echo -n ''
+  virsh vol-delete baremetalbrbm_brbm1_${i}.qcow2 default 2> /dev/null
   rm -f /var/lib/libvirt/images/baremetalbrbm_brbm1_${i}.qcow2 2> /dev/null
 done
+echo "Cleanup Completed"
