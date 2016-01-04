84fe6dd123cbb494fa0899ea260cebcf67b0387e
Dan Radez <dradez@redhat.com>
2016-01-04 15:49:26 -0500

    updating vm creation for virt deployment
    - replacing brbm with logical names br-netname
    - replacing instack-virt-setup with tripleo scripts
    
    JIRA: APEX-90, APEX-80
    
    Change-Id: I58a15dee8de882e034c8af8a3368ca0647741b13
    Signed-off-by: Dan Radez <dradez@redhat.com>

---
+++ build/variables.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+##############################################################################
+# Copyright (c) 2015 Tim Rozet (Red Hat), Dan Radez (Red Hat) and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+#rdo_images_uri=https://ci.centos.org/artifacts/rdo/images/mitaka/delorean/stable/
+rdo_images_uri=file:///stable-images/
+#onos_artifacts_uri=http://205.177.226.237:9999/onosfw
+onos_artifacts_uri=file:///stable-images/onos/
+
