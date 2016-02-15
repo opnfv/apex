3d6bf8c6245310de659ee5e9ae527c2d53493e7a
Dan Radez <dradez@redhat.com>
2016-02-15 18:49:00 -0500

    Adding SDNVPN support
    Change-Id: Id2d6d46a53129d4ae2f93fad835536da067e42df
    JIRA: APEX-51
    Signed-off-by: Dan Radez <dradez@redhat.com>
    
    Conflicts:
    	build/instack.sh

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -303,7 +303,7 @@ parse_inventory_file() {
     exit 1
   fi
 
-  eval $(parse_yaml $INVENTORY_FILE) || ( echo "${red}Failed to parse inventory.yaml. Aborting.${reset}" && exit 1 )
+  eval $(parse_yaml $INVENTORY_FILE) || { echo "${red}Failed to parse inventory.yaml. Aborting.${reset}" && exit 1 }
 
   instack_env_output="
 {
@@ -798,6 +798,8 @@ function undercloud_prep_overcloud_deploy {
       DEPLOY_OPTIONS+=" -e /usr/share/openstack-tripleo-heat-templates/environments/opendaylight_l3.yaml"
     elif [ "${deploy_options_array['sfc']}" == 'true' ]; then
       DEPLOY_OPTIONS+=" -e /usr/share/openstack-tripleo-heat-templates/environments/opendaylight_sfc.yaml"
+    elif [ "${deploy_options_array['vpn']}" == 'true' ]; then
+      DEPLOY_OPTIONS+=" -e /usr/share/openstack-tripleo-heat-templates/environments/opendaylight_sdnvpn.yaml"
     else
       DEPLOY_OPTIONS+=" -e /usr/share/openstack-tripleo-heat-templates/environments/opendaylight.yaml"
     fi
@@ -881,6 +883,12 @@ function undercloud_prep_overcloud_deploy {
 if [ "$debug" == 'TRUE' ]; then
     LIBGUESTFS_BACKEND=direct virt-customize -a overcloud-full.qcow2 --root-password password:opnfvapex
 fi
+if [ "${deploy_options_array['vpn']}" == 'true' ]; then
+    # append /etc/neutron/networking_bgpvpn.conf to neutron.conf
+    # for some reason the networking_bgpvpn.conf file doesn't get picked up
+    LIBGUESTFS_BACKEND=direct virt-customize -a overcloud-full.qcow2 --run-command "cat /etc/neutron/networking_bgpvpn.conf  >> /etc/neutron/neutron.conf "
+fi
+
 source stackrc
 set -o errexit
 echo "Uploading overcloud glance images"
