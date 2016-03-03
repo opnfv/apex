1f2de5fb656e1aca5aa749735171d5d491f6b90b
Dan Radez <dradez@redhat.com>
2016-03-03 18:06:16 -0500

    Deploy ceph OSDs on the compute nodes too
    opnfv-tht-pr: 7
    
    JIRA: APEX-89
    
    Change-Id: I5c27209ed006309285bd24d77cc2392513325116
    Signed-off-by: Dan Radez <dradez@redhat.com>

--- build/undercloud.sh
+++ build/undercloud.sh
@@ -22,8 +22,7 @@ pushd images > /dev/null
 # OpenWSMan package update supports the AMT Ironic driver for the TealBox
 LIBGUESTFS_BACKEND=direct virt-customize \
     --run-command "sed -i '/ControllerEnableCephStorage/c\\  ControllerEnableCephStorage: true' /usr/share/openstack-tripleo-heat-templates/environments/storage-environment.yaml" \
-    --run-command "sed -i '/  \$enable_ceph = /c\\  \$enable_ceph = true' /usr/share/openstack-tripleo-heat-templates/puppet/manifests/overcloud_controller_pacemaker.pp" \
-    --run-command "sed -i '/  \$enable_ceph = /c\\  \$enable_ceph = true' /usr/share/openstack-tripleo-heat-templates/puppet/manifests/overcloud_controller.pp" \
+    --run-command "sed -i '/ComputeEnableCephStorage/c\\  ComputeEnableCephStorage: true' /usr/share/openstack-tripleo-heat-templates/environments/storage-environment.yaml" \
     --run-command "curl http://download.opensuse.org/repositories/Openwsman/CentOS_CentOS-7/Openwsman.repo > /etc/yum.repos.d/wsman.repo" \
     --run-command "yum update -y openwsman*" \
     --run-command "cp /usr/share/instack-undercloud/undercloud.conf.sample /home/stack/undercloud.conf && chown stack:stack /home/stack/undercloud.conf" \
