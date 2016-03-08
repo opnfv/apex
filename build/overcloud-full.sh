06cdd24cf30752943bd9fdd97ae675600632e73a
Dan Radez <dradez@redhat.com>
2016-03-08 16:19:04 -0500

    Migating to Mitaka, Only NOSDN for this patch
    JIRA: APEX-108, APEX-93, APEX-92
    
    Change-Id: Ic1a24baec7ef295e9dc1b8b72ac36ca5b05578ea
    Signed-off-by: Dan Radez <dradez@redhat.com>

--- build/overcloud-full.sh
+++ build/overcloud-full.sh
@@ -22,42 +22,11 @@ mv -f images/overcloud-full.qcow2 images/overcloud-full_build.qcow2
 ##########################################################
 
 pushd images > /dev/null
-# Update puppet-aodh it's old
-rm -rf aodh
-git clone https://github.com/openstack/puppet-aodh aodh
-pushd aodh > /dev/null
-git checkout stable/liberty
-popd > /dev/null
-tar -czf puppet-aodh.tar.gz aodh
 
-# grab latest puppet-neutron module
-rm -rf puppet-neutron
-git clone -b stable/liberty https://github.com/openstack/puppet-neutron.git
-pushd puppet-neutron
-git archive --format=tar.gz --prefix=neutron/ HEAD > ../puppet-neutron.tar.gz
-popd
-
-
-# Add epel to install ceph
-# update aodh
-# puppet-neutron-force-metadata.patch
-# puppet-cinder-quota-fix
-#aodh-puppet-tripleo
-AODH_PKG="openstack-aodh-api,openstack-aodh-common,openstack-aodh-compat,openstack-aodh-evaluator,openstack-aodh-expirer"
-AODH_PKG+=",openstack-aodh-listener,openstack-aodh-notifier"
+# remove openstack-neutron-openvswitch, ain't nobody need that in OPNFV
+# enable connection tracking for protocal sctp
 LIBGUESTFS_BACKEND=direct virt-customize \
-    --upload puppet-aodh.tar.gz:/etc/puppet/modules/ \
-    --run-command "cd /etc/puppet/modules/ && rm -rf aodh && tar xzf puppet-aodh.tar.gz" \
     --run-command "echo 'nf_conntrack_proto_sctp' > /etc/modules-load.d/nf_conntrack_proto_sctp.conf" \
-    --install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm \
-    --install "$AODH_PKG,ceph" \
-    --run-command "rm -rf /etc/puppet/modules/neutron" \
-    --upload puppet-neutron.tar.gz:/etc/puppet/modules/ \
-    --run-command "cd /etc/puppet/modules/ && tar xzf puppet-neutron.tar.gz" \
-    --upload ../puppet-cinder-quota-fix.patch:/tmp \
-    --run-command "cd /etc/puppet/modules/cinder && patch -Np1 < /tmp/puppet-cinder-quota-fix.patch" \
-    --upload ../aodh-puppet-tripleo.patch:/tmp \
-    --run-command "cd /etc/puppet/modules/tripleo && patch -Np1 < /tmp/aodh-puppet-tripleo.patch" \
     -a overcloud-full_build.qcow2
 
 mv -f overcloud-full_build.qcow2 overcloud-full.qcow2
