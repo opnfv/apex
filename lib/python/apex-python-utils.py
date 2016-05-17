cf94b61421b5cdcc7b5a9fc635ce2c0c02f240bd
Dan Radez <dradez@redhat.com>
2016-05-16 20:01:31 -0400

    Converting network-environment file updates to pyyaml
    Change-Id: I419f02e9d8f92057b66bd193387a4ef673b1c692
    Signed-off-by: Dan Radez <dradez@redhat.com>

--- lib/python/apex-python-utils.py
+++ lib/python/apex-python-utils.py
@@ -13,12 +13,16 @@ import sys
 import apex
 import logging
 import os
+import yaml
+
 from jinja2 import Environment, FileSystemLoader
+from apex.net_env import PRIVATE_NETWORK, STORAGE_NETWORK, API_NETWORK
 
 
 def parse_net_settings(settings_args):
-    settings = apex.NetworkSettings(settings_args.path,
-                                    settings_args.network_isolation)
+    settings = apex.NetworkSettings()
+    settings.load(settings_args.path,
+                  settings_args.network_isolation)
     settings.dump_bash()
 
 
@@ -30,12 +34,69 @@ def find_ip(int_args):
 
 
 def build_nic_template(nic_args):
+    """
+    Render and print a network template
+    """
     env = Environment(loader=FileSystemLoader(nic_args.template_directory))
     template = env.get_template(nic_args.template_filename)
     print(template.render(enabled_networks=nic_args.enabled_networks,
                           external_net_type=nic_args.ext_net_type,
                           external_net_af=nic_args.address_family))
 
+def build_netenv_template(args):
+    """
+    Load the network-environment.yaml file and modify its contents
+    then pretty print the yaml output
+    """
+    netsets = apex.NetworkSettings()
+    netsets.load_pickle64(args.network_settings)
+    settings = netsets.settings_obj
+    with open('/'.join([args.template_directory, args.template_filename]), 'r') as stream:
+        netenv = yaml.load(stream)
+
+    # update admin/provisioning and public/external network settings
+    netenv['parameter_defaults']['ControlPlaneSubnetCidr'] = settings['admin_network']['cidr'].prefixlen
+    netenv['parameter_defaults']['ControlPlaneDefaultRoute'] = settings['admin_network']['provisioner_ip']
+    netenv['parameter_defaults']['ExternalNetCidr'] = str(settings['public_network']['cidr'])
+    netenv['parameter_defaults']['ExternalAllocationPools'] = [{'start': settings['public_network']['usable_ip_range'].split(',')[0],
+                                                                'end': settings['public_network']['usable_ip_range'].split(',')[1]}]
+    netenv['parameter_defaults']['ExternalInterfaceDefaultRoute'] = settings['public_network']['gateway']
+    netenv['parameter_defaults']['EC2MetadataIp'] = settings['admin_network']['provisioner_ip']
+    # dns not implimented yet
+    #netenv['parameter_defaults']['DnsServers'] = settings.DNSSERVERS?
+    #DnsServers: ["8.8.8.8","8.8.4.4"]
+
+    # check for the others
+    if PRIVATE_NETWORK in args.enabled_networks:
+        # resource registry updates
+        for i in ['OS::TripleO::Network::Tenant',
+                  'OS::TripleO::Controller::Ports::TenantPort',
+                  'OS::TripleO::Compute::Ports::TenantPort']:
+            netenv['resource_registry'][i] = netenv['resource_registry'][i].replace('noop', 'tenant')
+        # default parameter updates
+        netenv['parameter_defaults']['TenantNetCidr'] = str(settings.private_network.cidr)
+        netenv['parameter_defaults']['TenantAllocationPools'] = [{'start': settings.private_network.usable_ip_range.split(',')[0],
+                                                                  'end': settings.private_network.usable_ip_range.split(',')[1]}]
+
+    if STORAGE_NETWORK in args.enabled_networks:
+        # resource registry updates
+        for i in ['OS::TripleO::Network::Storage',
+                  'OS::TripleO::Network::Ports::StorageVipPort',
+                  'OS::TripleO::Controller::Ports::StoragePort',
+                  'OS::TripleO::Compute::Ports::StoragePort']:
+            netenv['resource_registry'][i] = netenv['resource_registry'][i].replace('noop', 'storage')
+
+        # default parameter updates
+        netenv['parameter_defaults']['StorageNetCidr'] = str(settings.storage_network.cidr)
+        netenv['parameter_defaults']['StorageAllocationPools'] = [{'start': settings.storage_network.usable_ip_range.split(',')[0],
+                                                                   'end': settings.storage_network.usable_ip_range.split(',')[1]}]
+
+    if API_NETWORK in args.enabled_networks:
+        # API_NETWORK not implimented here
+        # needed for IPv6?
+        pass
+
+    print(yaml.dump(netenv, default_flow_style=False))
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--DEBUG', action='store_true', default=False,
@@ -72,6 +133,17 @@ nic_template.add_argument('-e', '--ext_net_type', default='interface',
 nic_template.add_argument('-af', '--address_family', type=int, default=4,
                           help='IP address family')
 nic_template.set_defaults(func=build_nic_template)
+
+netenv_template = subparsers.add_parser('netenv-template', help='Build NIC templates')
+netenv_template.add_argument('-d', '--template-directory', dest='template_directory', required=True,
+                             help='Template file directory')
+netenv_template.add_argument('-f', '--template-filename', dest='template_filename', required=True,
+                             help='Template file to process')
+netenv_template.add_argument('--network-settings', dest='network_settings', required=True,
+                             help='A pickled and base64 encoded NetworkSettings object')
+netenv_template.add_argument('-n', '--enabled_networks', required=True,
+                             help='enabled network list')
+netenv_template.set_defaults(func=build_netenv_template)
 
 args = parser.parse_args(sys.argv[1:])
 if args.DEBUG:
