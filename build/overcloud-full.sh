e40f20365eecba6ebb093b3627de4e73d98a1292
Dan Radez <dradez@redhat.com>
2016-05-26 13:41:24 -0400

    [WIP] Adding congress support
    JIRA: APEX-135
    
    opnfv-tht-pr: 16
    
    Change-Id: Ic14abd13e2aa46d7ef37b74dd7b005ebf9977bdd
    Signed-off-by: Dan Radez <dradez@redhat.com>

--- build/overcloud-full.sh
+++ build/overcloud-full.sh
@@ -12,6 +12,8 @@ source ./cache.sh
 source ./variables.sh
 
 populate_cache "$rdo_images_uri/overcloud-full.tar"
+populate_cache "$openstack_congress"
+populate_cache "$python_congressclient"
 
 if [ ! -d images/ ]; then mkdir images; fi
 tar -xf cache/overcloud-full.tar -C images/
@@ -23,18 +25,24 @@ mv -f images/overcloud-full.qcow2 images/overcloud-full_build.qcow2
 
 pushd images > /dev/null
 
-# remove openstack-neutron-openvswitch, ain't nobody need that in OPNFV
+# tar up the congress puppet module
+rm -rf puppet-congress
+git clone https://github.com/opnfv/puppet-congress
+pushd puppet-congress > /dev/null
+git archive --format=tar.gz --prefix=congress/ HEAD > ../puppet-congress.tar.gz
+popd > /dev/null
+
 # enable connection tracking for protocal sctp
+# add CPU pinning script
+# install the congress rpms
+# upload and explode the congress puppet module
 LIBGUESTFS_BACKEND=direct virt-customize \
     --run-command "echo 'nf_conntrack_proto_sctp' > /etc/modules-load.d/nf_conntrack_proto_sctp.conf" \
-    -a overcloud-full_build.qcow2
-
-###################################
-#####  Add CPU pinning script #####
-###################################
-
-LIBGUESTFS_BACKEND=direct virt-customize \
     --upload ../setkernelparam.sh:/root \
+    --install "$openstack_congress" \
+    --install "$python_congressclient" \
+    --upload puppet-congress.tar.gz:/etc/puppet/modules/ \
+    --run-command "cd /etc/puppet/modules/ && tar xzf puppet-congress.tar.gz" \
     -a overcloud-full_build.qcow2
 
 mv -f overcloud-full_build.qcow2 overcloud-full.qcow2
