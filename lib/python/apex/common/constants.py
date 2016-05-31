68fc16c4fc98d26bbb58cda6ce7945c2ac6d79bc
Tim Rozet <trozet@redhat.com>
2016-05-31 17:40:11 -0400

    Migrates network environment parsing to python
    Change-Id: Ibaf20a2960a9f4b5e3f256fbed12a61d7606a967
    Signed-off-by: Tim Rozet <trozet@redhat.com>

--- lib/python/apex/common/constants.py
+++ lib/python/apex/common/constants.py
@@ -1,5 +1,5 @@
 ##############################################################################
-# Copyright (c) 2016 Feng Pan (fpan@redhat.com) and others.
+# Copyright (c) 2016 Tim Rozet (trozet@redhat.com) and others.
 #
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Apache License, Version 2.0
@@ -7,7 +7,10 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 
-
-from .network_settings import NetworkSettings
-from .deploy_env import DeploySettings
-from .network_environment import NetworkEnvironment
+ADMIN_NETWORK = 'admin_network'
+PRIVATE_NETWORK = 'private_network'
+PUBLIC_NETWORK = 'public_network'
+STORAGE_NETWORK = 'storage_network'
+API_NETWORK = 'api_network'
+OPNFV_NETWORK_TYPES = [ADMIN_NETWORK, PRIVATE_NETWORK, PUBLIC_NETWORK,
+                       STORAGE_NETWORK, API_NETWORK]
