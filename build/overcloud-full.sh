6db826c1ac5cdc44a6abf264095a487a2bab60a8
Michael Chapman <woppin@gmail.com>
2016-06-02 01:13:50 +1000

    WIP: Add dpdk packages to overcloud
    Include dpdk and ovs-dpdk packages in overcloud image.
    When the dataplane is specified as ovs_dpdk, install packages
    to replace ovs.
    
    opnfv-tht-pr: 17
    
    Change-Id: I595e39e9ddb2fce8c99220d9f656d1dedcf3548c
    Signed-off-by: Michael Chapman <woppin@gmail.com>

--- build/overcloud-full.sh
+++ build/overcloud-full.sh
@@ -37,5 +37,38 @@ LIBGUESTFS_BACKEND=direct virt-customize \
     --upload ../setkernelparam.sh:/root \
     -a overcloud-full_build.qcow2
 
+###############################
+#####  Add OVS DPDK Tools #####
+###############################
+# michchap: I don't know if these are needed, the puppet is doing a source build+install so may
+#           be counterproductive
+
+rm -rf networking-ovs-dpdk
+git clone -b stable/mitaka https://github.com/openstack/networking-ovs-dpdk
+pushd networking-ovs-dpdk > /dev/null
+git archive --format=tar.gz --prefix=networking-ovs-dpdk/ HEAD > ../networking-ovs-dpdk.tar.gz
+popd > /dev/null
+LIBGUESTFS_BACKEND=direct virt-customize --run-command "rm -rf /usr/share/apex-dpdk" \
+                                         --run-command "mkdir -p /usr/share/apex-dpdk" \
+                                         --upload networking-ovs-dpdk.tar.gz:/usr/share/apex-dpdk \
+                                         --run-command "cd /usr/share/apex-dpdk && tar xzf networking-ovs-dpdk.tar.gz" \
+                                         --run-command "ln -s /usr/share/apex-dpdk/networking-ovs-dpdk/puppet/ovsdpdk /etc/puppet/modules/ovsdpdk" \
+                                         -a overcloud-full_build.qcow2
+
+# Upload the dpdk + ovs-dpdk rpms, but don't install it since it will
+# replace OVS. The install should be done at deploy time when we
+# know if the option is set
+guestfs_string=''
+
+for package in ${ovsdpdk_rpms[@]} $ovsdpdk_ovs_rpm; do
+  rm -rf $package
+  curl -O "$ovsdpdk_uri_base/$package"
+  guestfs_string+=" --upload $package:/usr/share/apex-dpdk"
+done
+
+guestfs_string+=" -a overcloud-full_build.qcow2"
+
+LIBGUESTFS_BACKEND=direct virt-customize $guestfs_string
+
 mv -f overcloud-full_build.qcow2 overcloud-full.qcow2
 popd > /dev/null
