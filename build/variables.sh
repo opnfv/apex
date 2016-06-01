82fa04ef4493233cec0e900ae4af0354fdee362f
Michael Chapman <woppin@gmail.com>
2016-06-02 01:13:50 +1000

    WIP: Add dpdk packages to overcloud
    Include dpdk and ovs-dpdk packages in overcloud image.
    When the dataplane is specified as ovs_dpdk, install packages
    to replace ovs.
    
    opnfv-tht-pr: 17
    
    Change-Id: I595e39e9ddb2fce8c99220d9f656d1dedcf3548c
    Signed-off-by: Michael Chapman <woppin@gmail.com>

--- build/variables.sh
+++ build/variables.sh
@@ -11,3 +11,13 @@
 rdo_images_uri=https://ci.centos.org/artifacts/rdo/images/mitaka/delorean/stable/
 onos_release_uri=https://dl.dropboxusercontent.com/u/7079970/
 onos_release_file=onos-1.5.0.tar.gz
+
+# OVS DPDK
+ovsdpdk_uri_base=http://artifacts.opnfv.org/ovsnfv
+ovsdpdk_ovs_rpm=ovs4opnfv-openvswitch-2.5.90-0.12060.git46ed1382.1.el7.centos.x86_64.rpm
+ovsdpdk_rpms=(
+'ovs4opnfv-dpdk-16.04.0-1.el7.centos.x86_64.rpm'
+'ovs4opnfv-dpdk-devel-16.04.0-1.el7.centos.x86_64.rpm'
+'ovs4opnfv-dpdk-examples-16.04.0-1.el7.centos.x86_64.rpm'
+'ovs4opnfv-dpdk-tools-16.04.0-1.el7.centos.x86_64.rpm'
+)
