6297c45a97ce373689db33a411d8bc904a20a634
Feng Pan <fpan@redhat.com>
2016-06-10 09:01:30 -0400

    Install dpdk packages when dataplane is set to ovs_dpdk
    Change-Id: Ib9978595738bb70172244af0505b6b43b0bc60a2
    Signed-off-by: Feng Pan <fpan@redhat.com>

--- build/variables.sh
+++ build/variables.sh
@@ -11,3 +11,11 @@
 rdo_images_uri=https://ci.centos.org/artifacts/rdo/images/mitaka/delorean/stable/
 onos_release_uri=https://dl.dropboxusercontent.com/u/7079970/
 onos_release_file=onos-1.5.0.tar.gz
+
+dpdk_uri_base=http://artifacts.opnfv.org/ovsnfv
+dpdk_rpms=(
+'ovs4opnfv-dpdk-16.04.0-1.el7.centos.x86_64.rpm'
+'ovs4opnfv-dpdk-devel-16.04.0-1.el7.centos.x86_64.rpm'
+'ovs4opnfv-dpdk-examples-16.04.0-1.el7.centos.x86_64.rpm'
+'ovs4opnfv-dpdk-tools-16.04.0-1.el7.centos.x86_64.rpm'
+)
