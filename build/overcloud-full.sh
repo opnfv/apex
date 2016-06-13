af2f93cebc62cd0d6f7c4b1a74fdeb65fbcf90a8
Dan Radez <dradez@redhat.com>
2016-06-13 14:56:16 -0400

    puppet-tripleo fork integration
    moving the github integration with pull requests
    into a function so we can continue to service opnfv-tht
    and also service opnfv-puppet-tripleo with the same code
    
    opnfv-puppet-tripleo-pr: 3
    
    Change-Id: Ief2b1e7309defce9ed2029f6baade3c1371b7d96
    Signed-off-by: Dan Radez <dradez@redhat.com>

--- build/overcloud-full.sh
+++ build/overcloud-full.sh
@@ -10,6 +10,7 @@
 set -e
 source ./cache.sh
 source ./variables.sh
+source ./functions.sh
 
 populate_cache "$rdo_images_uri/overcloud-full.tar"
 
@@ -21,6 +22,12 @@ mv -f images/overcloud-full.qcow2 images/overcloud-full_build.qcow2
 #####  Prep initial overcloud image with common deps #####
 ##########################################################
 
+# prep opnfv-puppet-tripleo for undercloud
+clone_fork opnfv-puppet-tripleo
+pushd opnfv-puppet-tripleo > /dev/null
+git archive --format=tar.gz --prefix=tripleo/ HEAD > ../opnfv-puppet-tripleo.tar.gz
+popd > /dev/null
+
 pushd images > /dev/null
 
 dpdk_pkg_str=''
@@ -29,10 +36,12 @@ for package in ${dpdk_rpms[@]}; do
   dpdk_pkg_str+=" --upload $package:/root/dpdk_rpms"
 done
 
-# remove openstack-neutron-openvswitch, ain't nobody need that in OPNFV
+# installing forked opnfv-puppet-tripleo
 # enable connection tracking for protocal sctp
 # upload dpdk rpms but do not install
 LIBGUESTFS_BACKEND=direct virt-customize \
+    --upload ../opnfv-puppet-tripleo.tar.gz:/etc/puppet/modules \
+    --run-command "cd /etc/puppet/modules && rm -rf tripleo && tar xzf opnfv-puppet-tripleo.tar.gz" \
     --run-command "echo 'nf_conntrack_proto_sctp' > /etc/modules-load.d/nf_conntrack_proto_sctp.conf" \
     --run-command "mkdir /root/dpdk_rpms" \
     $dpdk_pkg_str \
