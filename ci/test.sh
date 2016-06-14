2c722f47a9dc626584350758ab97577084e6a479
Tim Rozet <trozet@redhat.com>
2016-06-14 10:06:05 -0400

    Fixes unit tests
    Change-Id: If6a8366dec49cff85ba94c13e70a1796e7b6ca47
    Signed-off-by: Tim Rozet <trozet@redhat.com>

--- ci/test.sh
+++ ci/test.sh
@@ -10,7 +10,7 @@
 
 set -e
 
-# Make sure python is installed
+# Make sure python dependencies are installed
 if ! rpm -q python34-devel > /dev/null; then
     sudo yum install -y epel-release
     if ! sudo yum install -y python34-devel; then
@@ -19,10 +19,19 @@ if ! rpm -q python34-devel > /dev/null; then
     fi
 fi
 
+if ! rpm -q python34-nose > /dev/null; then
+    if ! sudo yum install -y python34-nose; then
+        echo "Failed to install python34-nose package..."
+        exit 1
+    fi
+fi
+
 # Make sure coverage is installed
 if ! python3 -c "import coverage" &> /dev/null; then sudo easy_install-3.4 coverage; fi
 
+pushd ../build/ > /dev/null
 make python-tests
+popd > /dev/null
 pushd ../tests/ > /dev/null
 percent=$(coverage3 report --include '*lib/python/*' -m | grep TOTAL | tr -s ' ' | awk '{ print $4 }' | cut -d % -f 1)
 if [[ percent -lt 80 ]]; then
@@ -30,4 +39,4 @@ if [[ percent -lt 80 ]]; then
     echo "Does not meet 80% requirement"
     exit 1
 fi
-popd > /dev/nul
+popd > /dev/null
