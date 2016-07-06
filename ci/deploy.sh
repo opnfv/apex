033ab8faecab777de7054670c4ad67fc2d33d398
Dan Radez <dradez@redhat.com>
2016-07-06 09:35:07 -0400

    Splitting out common functions to lib files
    trying to reduce the monolith deploy.sh to smaller parts
    
    Change-Id: Ie2d2d74de5b8cacbb1c57d3d397bede0b659c0af
    Signed-off-by: Dan Radez <dradez@redhat.com>

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -62,6 +62,7 @@ $LIB/configure-deps-functions.sh
 $LIB/parse-functions.sh
 $LIB/virtual-setup-functions.sh
 $LIB/undercloud-functions.sh
+$LIB/overcloud-deploy-functions.sh
 $LIB/post-install-functions.sh
 $LIB/utility-functions.sh
 $LIB/installer/onos/onos_gw_mac_update.sh
@@ -72,37 +73,6 @@ for lib_file in ${lib_files[@]}; do
     exit 1
   fi
 done
-
-##FUNCTIONS
-##checks if prefix exists in string
-##params: string, prefix
-##usage: contains_prefix "deploy_setting_launcher=1" "deploy_setting"
-contains_prefix() {
-  local mystr=$1
-  local prefix=$2
-  if echo $mystr | grep -E "^$prefix.*$" > /dev/null; then
-    return 0
-  else
-    return 1
-  fi
-}
-
-##verify internet connectivity
-#params: none
-function verify_internet {
-  if ping -c 2 $ping_site > /dev/null; then
-    if ping -c 2 www.google.com > /dev/null; then
-      echo "${blue}Internet connectivity detected${reset}"
-      return 0
-    else
-      echo "${red}Internet connectivity detected, but DNS lookup failed${reset}"
-      return 1
-    fi
-  else
-    echo "${red}No internet connectivity detected${reset}"
-    return 1
-  fi
-}
 
 display_usage() {
   echo -e "Usage:\n$0 [arguments] \n"
@@ -238,8 +208,6 @@ parse_cmdline() {
   fi
 
 }
-
-##END FUNCTIONS
 
 main() {
   parse_cmdline "$@"
