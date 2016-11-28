05f8918243b3579a683f1dd49fa06bff8b046274
Tim Rozet <trozet@redhat.com>
2016-11-28 15:03:04 -0500

    Updates VPP packages to 16.12 version
    JIRA: APEX-344
    
    Change-Id: Ib1bf1ac6b76af3a4cbbc2b7a20753c232ff5fa16
    Signed-off-by: Tim Rozet <trozet@redhat.com>

--- build/variables.sh
+++ build/variables.sh
@@ -27,12 +27,3 @@ dpdk_rpms=(
 'ovs4opnfv-e8acab14-dpdk-tools-16.04.0-1.el7.centos.x86_64.rpm'
 'ovs4opnfv-e8acab14-openvswitch-2.5.90-0.12032.gitc61e93d6.1.el7.centos.x86_64.rpm'
 )
-
-fdio_uri_base=http://artifacts.opnfv.org/apex/colorado
-fdio_pkgs=(
-'vpp-16.09-release.x86_64.rpm'
-'vpp-devel-16.09-release.x86_64.rpm'
-'vpp-lib-16.09-release.x86_64.rpm'
-'vpp-python-api-16.09-release.x86_64.rpm'
-)
-honeycomb_pkg='honeycomb-1.16.9-FINAL.noarch.rpm'
