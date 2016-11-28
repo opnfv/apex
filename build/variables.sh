7c3da5d39dbf71429b0ac31c8b790e102031d645
Tim Rozet <trozet@redhat.com>
2016-11-28 15:03:04 -0500

    Updates VPP packages to latest master version
    JIRA: APEX-344
    
    Change-Id: Ib1bf1ac6b76af3a4cbbc2b7a20753c232ff5fa16
    Signed-off-by: Tim Rozet <trozet@redhat.com>
    (cherry picked from commit c5743c31f5018d34f756a2c48dd5e7f6948629e3)

--- build/variables.sh
+++ build/variables.sh
@@ -27,15 +27,6 @@ dpdk_rpms=(
 'ovs4opnfv-e8acab14-openvswitch-2.5.90-0.12032.gitc61e93d6.1.el7.centos.x86_64.rpm'
 )
 
-fdio_uri_base=http://artifacts.opnfv.org/apex/colorado
-fdio_pkgs=(
-'vpp-16.09-release.x86_64.rpm'
-'vpp-devel-16.09-release.x86_64.rpm'
-'vpp-lib-16.09-release.x86_64.rpm'
-'vpp-python-api-16.09-release.x86_64.rpm'
-)
-honeycomb_pkg='honeycomb-1.16.9-FINAL.noarch.rpm'
-
 virt_uri_base=https://people.redhat.com/~rjones/libguestfs-RHEL-7.3-preview
 libguestfs_pkg='libguestfs-1.32.7-3.el7.x86_64.rpm'
 virt_pkgs=(
