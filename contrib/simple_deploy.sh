7cd3e80f8d34cb998d1cae1af58afdda406d5a3b
Nikolas Hermanns <nikolas.hermanns@ericsson.com>
2016-12-01 12:51:28 -0500

    Adding an easy install Script
    This script is for new commers. It deployes apex in the most
    easiest way. People can just execute it and see what happens.
    As well the script is so short and easy that they directly
    see what needs to be done.
    
    Change-Id: I02c9773ad6df154c735024de2ffd02254c8f67bb
    Signed-off-by: Nikolas Hermanns <nikolas.hermanns@ericsson.com>

---
+++ contrib/simple_deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+apex_home=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+export CONFIG=$apex_home/build
+export LIB=$apex_home/lib
+export RESOURCES=$apex_home/.build/images/
+export PYTHONPATH=$PYTHONPATH:$apex_home/lib/python
+$apex_home/ci/dev_dep_check.sh
+pushd $apex_home/build
+make clean
+make undercloud
+make overcloud-opendaylight
+pushd $apex_home/ci
+./clean.sh
+./dev_dep_check.sh
+out=/tmp/opnfv-deploy.out
+echo "All further output will be piped to $out"
+(nohup ./deploy.sh -v -n $apex_home/config/network/network_settings.yaml -d $apex_home/config/deploy/os-odl_l3-nofeature-noha.yaml &> $out &)
+popd
