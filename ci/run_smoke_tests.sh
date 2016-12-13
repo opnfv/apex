f49aafa6406b17843026550a1abfd9a9f07f0fe7
spisarski <s.pisarski@cablelabs.com>
2016-12-13 14:37:23 -0700

    Adding installation of ansible and dependencies to host attempting to execute <apex>/ci/run_smoke_tests.sh. Also fixed the filename that contains the test stdout/stderr output from snoke-tests.out to smoke-tests.out
    JIRA: APEX-373
    
    Change-Id: I7ef68ae09c100e4e7466a10bb8e22279d9a6b41e
    Signed-off-by: spisarski <s.pisarski@cablelabs.com>

--- ci/run_smoke_tests.sh
+++ ci/run_smoke_tests.sh
@@ -4,5 +4,12 @@ source ../lib/utility-functions.sh
 
 export ANSIBLE_HOST_KEY_CHECKING=False
 
+./dev_dep_check.sh
+
+yum install python-devel -y
+yum install openssl-devel -y
+easy_install pip
+pip install ansible
+
 echo 'See ~stack/smoke-tests.out on the undercloud for result log'
 ansible-playbook -i "$(get_undercloud_ip)," ../tests/smoke_tests/smoke_tests.yml
