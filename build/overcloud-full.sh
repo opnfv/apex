2d5833e6895d0ee20eecfd16f67331bd1b0b0b91
Tim Rozet <trozet@redhat.com>
2017-01-03 17:08:37 -0500

    Removes vpp-plugins from build
    This may no longer be needed as a dependency to vpp and is breaking the
    build.
    
    Change-Id: I96df60bd4b33f6e6119359d560fd5fa771d858fa
    Signed-off-by: Tim Rozet <trozet@redhat.com>

--- build/overcloud-full.sh
+++ build/overcloud-full.sh
@@ -105,7 +105,7 @@ LIBGUESTFS_BACKEND=direct virt-customize \
     --run-command "mkdir /root/dpdk_rpms" \
     --upload ${BUILD_DIR}/fdio.repo:/etc/yum.repos.d/fdio.repo \
     $dpdk_pkg_str \
-    --run-command "yum install --downloadonly --downloaddir=/root/fdio vpp vpp-devel vpp-lib vpp-python-api vpp-plugins" \
+    --run-command "yum install --downloadonly --downloaddir=/root/fdio vpp vpp-devel vpp-lib vpp-python-api" \
     --upload ${BUILD_DIR}/noarch/$netvpp_pkg:/root/fdio \
     --run-command "yum install -y etcd" \
     --run-command "pip install python-etcd" \
