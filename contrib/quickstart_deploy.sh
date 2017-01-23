bb11614917e143f718fb47bf97b3103b85e7c094
Michael Chapman <woppin@gmail.com>
2017-01-24 03:03:39 +1100

    Add quickstart deploy script
    Add a script to the contrib directory that can deploy oooq using
    ovs for networks on the virthost.
    
    Change-Id: I2ef5dbb458a90ef3d135e52bf0879ab2df78fc8d
    Signed-off-by: Michael Chapman <woppin@gmail.com>

---
+++ contrib/quickstart_deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Usage: ./quickstart-deploy.sh [name_of_machine_to_host_VMs]
+#
+# Will fail if run as root, and must have passwordless SSH from
+# the current user to the root user of the target machine.
+#
+VIRTHOST=$1
+
+if  [ ! -d "tripleo-quickstart" ]; then
+  git clone https://github.com/openstack/tripleo-quickstart
+fi
+
+cd tripleo-quickstart
+git fetch origin
+git reset --hard origin/master
+
+# external net bridges
+git fetch git://git.openstack.org/openstack/tripleo-quickstart refs/changes/21/406421/8 && git cherry-pick FETCH_HEAD
+# ssh_user
+git fetch git://git.openstack.org/openstack/tripleo-quickstart refs/changes/03/422103/1 && git cherry-pick FETCH_HEAD
+# virtualport types
+git fetch git://git.openstack.org/openstack/tripleo-quickstart refs/changes/47/407347/5 && git cherry-pick FETCH_HEAD
+# run privileges
+git fetch git://git.openstack.org/openstack/tripleo-quickstart refs/changes/98/418198/8 && git cherry-pick FETCH_HEAD
+# opnfv config
+git fetch git://git.openstack.org/openstack/tripleo-quickstart refs/changes/93/422593/2 && git cherry-pick FETCH_HEAD
+
+bash quickstart.sh -p quickstart-extras.yml -r requirements.txt -r quickstart-extras-requirements.txt -c config/general_config/opnfv.yml --tags all --teardown all -n -X $VIRTHOST
+
