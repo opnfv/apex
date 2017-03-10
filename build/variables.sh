ae0fb8e3b4a490eaee82e7d58daaba568a3e7e70
Georg Kunz <georg.kunz@ericsson.com>
2017-03-10 11:50:07 +0100

    Final bump of Gluon to upstream release v1.0.1
    Change-Id: I7b220a332a8738054bcb569c3fa01b5d401a167e
    Signed-off-by: Georg Kunz <georg.kunz@ericsson.com>
    (cherry picked from commit 79dc043d32a1280b73f56a6f3ba458923e290874)

--- build/variables.sh
+++ build/variables.sh
@@ -56,7 +56,8 @@ netvpp_branch="master"
 netvpp_commit=$(git ls-remote ${netvpp_repo} ${netvpp_branch} | awk '{print substr($1,1,7)}')
 netvpp_pkg=python-networking-vpp-0.0.1-1.git${NETVPP_COMMIT}$(rpm -E %dist).noarch.rpm
 
-gluon_rpm=gluon-0.0.1-1_20170216.noarch.rpm
+gluon_rpm=gluon-0.0.1-1_20170302.noarch.rpm
+
 fdio_l2_uri_base=http://artifacts.opnfv.org/apex/danube/fdio_l2_rpms
 fdio_l2_pkgs=(
 'vpp-17.04-rc0~300_gdef19da~b1923.x86_64.rpm'
