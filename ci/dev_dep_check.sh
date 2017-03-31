3d6e0521a44fee6eee2340478ad2fa6e8469ad86
Dan Radez <dradez@redhat.com>
2017-03-30 23:03:43 -0400

    Updating Apex to OpenStack Ocata
    - power management updated to virtualbmc, pxe_ssh is deprecated
    - removing custom tacker build
    - removing custom congress build
    - disabling yum update in undercloud on the cli instead of in a patch
    - added undercloud installation completion percentage meter
    - Undercloud is direct kernel booted now, there are no kernel and initrd
      in the disk image from upstream
    
    Check TODOs in code for incomplete items
    
    JIRA: APEX-433
    JIRA: APEX-432
    JIRA: APEX-431
    
    Change-Id: I6963f16e65eacade5607a3082b58b6150331406c
    Signed-off-by: Dan Radez <dradez@redhat.com>

--- ci/dev_dep_check.sh
+++ ci/dev_dep_check.sh
@@ -32,8 +32,11 @@ if ! sudo yum update -y ipxe-roms-qemu; then
 fi
 
 # check for other packages
-for i in epel-release python34-PyYAML openvswitch openstack-tripleo libguestfs libguestfs-tools-c libvirt-python python2-oslo-config python2-debtcollector python34-devel libxslt-devel libxml2-devel; do
-# Make sure deploy deps are installed
+for i in epel-release python34-PyYAML openvswitch openstack-tripleo libguestfs \
+         libguestfs-tools-c libvirt-python python2-oslo-config \
+         python2-debtcollector python34-devel libxslt-devel \
+         libxml2-devel python-virtualbmc; do
+    # Make sure deploy deps are installed
     if ! rpm -q $i > /dev/null; then
         if ! sudo yum install -y $i; then
             echo "Failed to install $i package..."
@@ -48,9 +51,6 @@ sudo pip3 install python-ipmi
 
 # Make sure jinja2 is installed
 easy_install-3.4 jinja2
-
-# TODO(cgoncalves): remove once congress RPM is downloaded from upstream
-easy_install-3.4 tox
 
 # Required packages to redirect stdin with virt-customize
 if ! sudo yum -y install libguestfs libguestfs-tools libguestfs-tools-c supermin supermin5 supermin-helper perl-Sys-Guestfs python-libguestfs; then
