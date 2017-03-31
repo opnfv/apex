3e963e15a421bc6b33d5239d651660f6152387c8
Dan Radez <dradez@redhat.com>
2017-03-30 23:03:43 -0400

    Updating Apex to OpenStack Ocata
    - power management updated to virtualbmc, pxe_ssh is deprecated
    - removing custom tacker build
    - removing custom congress build
    - disabling yum update in undercloud on the cli instead of in a patch
    - Undercloud is direct kernel booted now, there are no kernel and initrd
      in the disk image from upstream
    - remove OpenDaylight previous to Carbon
    
    JIRA: APEX-433
    JIRA: APEX-432
    JIRA: APEX-431
    
    Change-Id: I6963f16e65eacade5607a3082b58b6150331406c
    Signed-off-by: Dan Radez <dradez@redhat.com>

--- ci/test.sh
+++ ci/test.sh
@@ -11,7 +11,7 @@
 set -e
 
 # Make sure python dependencies are installed
-for pkg in epel-release python34-devel python34-nose python-pep8; do
+for pkg in epel-release python34-devel python34-nose python-yaml python-pep8; do
   if ! rpm -q ${pkg} > /dev/null; then
     if ! sudo yum install -y ${pkg}; then
       echo "Failed to install ${pkg} package..."
