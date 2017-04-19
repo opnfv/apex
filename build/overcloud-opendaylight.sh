762ccea54e63eb710491a6da56f5eb3aaa8b8120
Tim Rozet <trozet@redhat.com>
2017-04-19 12:34:34 -0400

    Updates quagga to newer version
    Change-Id: I635b171c15bca40788eefe909eaab3e9d4606db1
    Signed-off-by: Tim Rozet <trozet@redhat.com>
    (cherry picked from commit a9dd0037f9e0d02e83c6f3f6fecb1c706c994878)

--- build/overcloud-opendaylight.sh
+++ build/overcloud-opendaylight.sh
@@ -74,7 +74,7 @@ git archive --format=tar.gz HEAD:deploy/puppet/ > ${BUILD_DIR}/puppet-gluon.tar.
 popd > /dev/null
 
 # Download quagga/zrpc rpms
-populate_cache http://artifacts.opnfv.org/apex/danube/quagga/quagga.tar.gz
+populate_cache http://artifacts.opnfv.org/apex/danube/quagga/quagga-2.tar.gz
 
 # Download ODL netvirt for VPP
 populate_cache http://artifacts.opnfv.org/apex/danube/fdio_netvirt/odl-netvirt-vpp-distribution.tar.gz
@@ -106,8 +106,8 @@ LIBGUESTFS_BACKEND=direct virt-customize \
     --install python-click \
     --upload ${CACHE_DIR}/$gluon_rpm:/root/\
     --install /root/$gluon_rpm \
-    --upload ${CACHE_DIR}/quagga.tar.gz:/root/ \
-    --run-command "cd /root/ && tar xzf quagga.tar.gz" \
+    --upload ${CACHE_DIR}/quagga-2.tar.gz:/root/ \
+    --run-command "cd /root/ && tar xzf quagga-2.tar.gz" \
     --run-command "yum downgrade -y python-zmq-14.3.1" \
     --install zeromq-4.1.4,zeromq-devel-4.1.4 \
     --install capnproto-devel,capnproto-libs,capnproto \
