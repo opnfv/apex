85b7b2b09f5a6b01b2d3b25e5b4ad9ffb5a2ea65
Nikolas Hermanns <nikolas.hermanns@ericsson.com>
2017-05-02 06:10:27 -0400

    fix no tty in post setup when using nohup
    Change-Id: Ia17276c2089a2f285cf6ce6db1a93e3aef4f83b4
    Signed-off-by: Nikolas Hermanns <nikolas.hermanns@ericsson.com>

--- contrib/simple_deploy.sh
+++ contrib/simple_deploy.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 set -e
 apex_home=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../
-export CONFIG=$apex_home/build
+scenario=$1
+if [[ ${scenario}x == x ]] || [ ! -e $apex_home/config/deploy/$scenario ];then
+  echo "Please give scenario as 1 argument. Unknown scenario: $scenario"
+  echo "Known: $(ls $apex_home/config/deploy/)"
+  exit 1
+fi
+
+export BASE=$apex_home/build
 export LIB=$apex_home/lib
 export RESOURCES=$apex_home/.build/
 export PYTHONPATH=$PYTHONPATH:$apex_home/lib/python
@@ -13,7 +20,9 @@ make undercloud
 make overcloud-opendaylight
 popd
 pushd $apex_home/ci
-echo "All further output will be piped to $PWD/nohup.out"
-(nohup ./deploy.sh -v -n $apex_home/config/network/network_settings.yaml -d $apex_home/config/deploy/os-odl_l3-nofeature-noha.yaml &)
-tail -f nohup.out
+out_file=/tmp/apex-simple-deploy.log
+echo "All further output will be piped to $out_file"
+rm -f $out_file
+screen -S deploy_apex bash -c "./deploy.sh -v -n $apex_home/config/network/network_settings.yaml -d $apex_home/config/deploy/$scenario --virtual-computes 3 2>&1 | tee $out_file; echo "Installation end! ctrl-c to lock out" >> $out_file"
+tail -f $out_file
 popd
