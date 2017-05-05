ed4309a1877727a5e46620f904fdb6d038b1b206
Dan Radez <dradez@redhat.com>
2017-05-05 16:51:02 -0400

    Adding conditionals for x86_64
    There are x86 specific build items that can't be used when building on
    aarch64. Adding conditionals so that the items only get built in when
    building on x86_64
    
    JIRA: APEX-381
    
    Change-Id: I63e1c99d5d22bbb523c88be7e973a6c834a38b01
    Signed-off-by: Dan Radez <dradez@redhat.com>

--- build/overcloud-full.sh
+++ build/overcloud-full.sh
@@ -36,42 +36,10 @@ pushd os-net-config/os_net_config > /dev/null
 git archive --format=tar.gz --prefix=os_net_config/ HEAD > ${BUILD_DIR}/os-net-config.tar.gz
 popd > /dev/null
 
-dpdk_pkg_str=''
-for package in ${dpdk_rpms[@]}; do
-  wget "$dpdk_uri_base/$package"
-  dpdk_pkg_str+=" --upload ${BUILD_DIR}/${package}:/root/dpdk_rpms"
-done
-
-# tar up the fd.io module
-rm -rf puppet-fdio
-git clone https://git.fd.io/puppet-fdio
-pushd puppet-fdio > /dev/null
-git archive --format=tar.gz --prefix=fdio/ HEAD > ${BUILD_DIR}/puppet-fdio.tar.gz
-popd > /dev/null
-
 # tar up vsperf
 rm -rf vsperf vsperf.tar.gz
 git clone https://gerrit.opnfv.org/gerrit/vswitchperf vsperf
 tar czf vsperf.tar.gz vsperf
-
-# Master FD.IO Repo
-cat > ${BUILD_DIR}/fdio.repo << EOF
-[fdio-master]
-name=fd.io master branch latest merge
-baseurl=https://nexus.fd.io/content/repositories/fd.io.master.centos7/
-enabled=1
-gpgcheck=0
-EOF
-
-# Get Real Time Kernel from kvm4nfv
-populate_cache $kvmfornfv_uri_base/$kvmfornfv_kernel_rpm
-
-# packages frozen for fdio scenarios
-fdio_pkg_str=''
-for package in ${fdio_pkgs[@]}; do
-  wget "$package"
-  fdio_pkg_str+=" --upload ${BUILD_DIR}/${package##*/}:/root/fdio/"
-done
 
 # Increase disk size by 900MB to accommodate more packages
 qemu-img resize overcloud-full_build.qcow2 +900MB
@@ -92,30 +60,69 @@ LIBGUESTFS_BACKEND=direct virt-customize \
     --run-command "if ! rpm -qa | grep python-redis; then yum install -y python-redis; fi" \
     --run-command "sed -i 's/^#UseDNS.*$/UseDNS no/' /etc/ssh/sshd_config" \
     --run-command "sed -i 's/^GSSAPIAuthentication.*$/GSSAPIAuthentication no/' /etc/ssh/sshd_config" \
+    --run-command "yum install -y etcd" \
+    --install python-etcd \
+    --run-command "puppet module install cristifalcas/etcd" \
+    --run-command "rm -f /etc/sysctl.d/80-vpp.conf" \
+    --install unzip \
+    --upload ${BUILD_DIR}/vsperf.tar.gz:/var/opt \
+    --run-command "cd /var/opt && tar xzf vsperf.tar.gz" \
+    --run-command "sed -i -E 's/timeout=[0-9]+/timeout=60/g' /usr/share/openstack-puppet/modules/rabbitmq/lib/puppet/provider/rabbitmqctl.rb" \
+    -a overcloud-full_build.qcow2
+
+# Arch dependent on x86
+if "$(uname -i)" == 'x86_64'; then
+
+dpdk_pkg_str=''
+for package in ${dpdk_rpms[@]}; do
+  wget "$dpdk_uri_base/$package"
+  dpdk_pkg_str+=" --upload ${BUILD_DIR}/${package}:/root/dpdk_rpms"
+done
+
+# tar up the fd.io module
+rm -rf puppet-fdio
+git clone https://git.fd.io/puppet-fdio
+pushd puppet-fdio > /dev/null
+git archive --format=tar.gz --prefix=fdio/ HEAD > ${BUILD_DIR}/puppet-fdio.tar.gz
+popd > /dev/null
+
+# Master FD.IO Repo
+cat > ${BUILD_DIR}/fdio.repo << EOF
+[fdio-master]
+name=fd.io master branch latest merge
+baseurl=https://nexus.fd.io/content/repositories/fd.io.master.centos7/
+enabled=1
+gpgcheck=0
+EOF
+
+# Get Real Time Kernel from kvm4nfv
+populate_cache $kvmfornfv_uri_base/$kvmfornfv_kernel_rpm
+
+# packages frozen for fdio scenarios
+fdio_pkg_str=''
+for package in ${fdio_pkgs[@]}; do
+  wget "$package"
+  fdio_pkg_str+=" --upload ${BUILD_DIR}/${package##*/}:/root/fdio/"
+done
+
+LIBGUESTFS_BACKEND=direct virt-customize \
     --run-command "mkdir /root/dpdk_rpms" \
     $dpdk_pkg_str \
+    --upload ${BUILD_DIR}/puppet-fdio.tar.gz:/etc/puppet/modules \
+    --run-command "cd /etc/puppet/modules && tar xzf puppet-fdio.tar.gz" \
     --run-command "mkdir /root/fdio" \
     --upload ${BUILD_DIR}/noarch/$netvpp_pkg:/root/fdio \
     $fdio_pkg_str \
     --run-command "yum install -y /root/fdio/*.rpm" \
-    --run-command "yum install -y etcd" \
-    --install python-etcd \
-    --run-command "puppet module install cristifalcas/etcd" \
     --install "centos-release-qemu-ev" \
     --run-command "yum install -y qemu-kvm-ev-2.3.0-31.el7_2.21.1.x86_64" \
     --run-command "yum remove -y qemu-system-x86" \
-    --run-command "rm -f /etc/sysctl.d/80-vpp.conf" \
-    --install unzip \
-    --upload ${BUILD_DIR}/puppet-fdio.tar.gz:/etc/puppet/modules \
-    --run-command "cd /etc/puppet/modules && tar xzf puppet-fdio.tar.gz" \
-    --upload ${BUILD_DIR}/vsperf.tar.gz:/var/opt \
-    --run-command "cd /var/opt && tar xzf vsperf.tar.gz" \
     --run-command "curl -f https://copr.fedorainfracloud.org/coprs/leifmadsen/ovs-master/repo/epel-7/leifmadsen-ovs-master-epel-7.repo > /etc/yum.repos.d/leifmadsen-ovs-master-epel-7.repo" \
     --run-command "mkdir /root/ovs27" \
     --run-command "yumdownloader --destdir=/root/ovs27 openvswitch*2.7* python-openvswitch-2.7*" \
-    --run-command "sed -i -E 's/timeout=[0-9]+/timeout=60/g' /usr/share/openstack-puppet/modules/rabbitmq/lib/puppet/provider/rabbitmqctl.rb" \
     --upload ${CACHE_DIR}/$kvmfornfv_kernel_rpm:/root/ \
     -a overcloud-full_build.qcow2
+fi
 
 mv -f overcloud-full_build.qcow2 overcloud-full.qcow2
 popd > /dev/null
