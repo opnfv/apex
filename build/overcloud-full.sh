29fafeb95bb33086861ce9baece5fb7bfd0ef126
Feng Pan <fpan@redhat.com>
2017-05-08 23:23:25 -0400

    Add type_drivers config to networking-vpp agent
    JIRA: APEX-459
    opnfv-puppet-tripleo-pr: 26
    
    Change-Id: Ie5595e5ad0304bab036b628543e4e3b3c7a38a2e
    Signed-off-by: Feng Pan <fpan@redhat.com>

--- build/overcloud-full.sh
+++ build/overcloud-full.sh
@@ -115,6 +115,8 @@ LIBGUESTFS_BACKEND=direct virt-customize \
     --run-command "yumdownloader --destdir=/root/ovs27 openvswitch*2.7* python-openvswitch-2.7*" \
     --run-command "sed -i -E 's/timeout=[0-9]+/timeout=60/g' /usr/share/openstack-puppet/modules/rabbitmq/lib/puppet/provider/rabbitmqctl.rb" \
     --upload ${CACHE_DIR}/$kvmfornfv_kernel_rpm:/root/ \
+    --upload ${BUILD_ROOT}/patches/puppet-neutron-vpp-ml2-type_drivers-setting.patch:/usr/share/openstack-puppet/modules/neutron/ \
+    --run-command "cd /usr/share/openstack-puppet/modules/neutron && patch -p1 <  puppet-neutron-vpp-ml2-type_drivers-setting.patch" \
     -a overcloud-full_build.qcow2
 
 mv -f overcloud-full_build.qcow2 overcloud-full.qcow2
