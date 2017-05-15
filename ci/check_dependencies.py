4840c0ea948137c942a7e091150130dcb81b3fbd
Tim Rozet <trozet@redhat.com>
2017-05-15 16:24:27 -0400

    Refactors build script for Apex
    Changes Include:
     - Build has been refactored into python
     - Caching now packs into multiple unique tar files which are
       logged into a journal file
     - Multiple files and journal allow for multiple apex builds to run
       simultaenously while minimizing risk of collision between cache
       reading and writing
     - Build dependency packages updated and will install when missing
       via python (Note yum package requires python2.x)
    
    Change-Id: Iae703253eb6034aee42bf33153bb6e434d3cec48
    Signed-off-by: Tim Rozet <trozet@redhat.com>

---
+++ ci/check_dependencies.py
@@ -0,0 +1,59 @@
+##############################################################################
+# Copyright (c) 2017 Tim Rozet (trozet@redhat.com) and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+import argparse
+import re
+import subprocess
+import sys
+import yum
+
+
+DEPENDENCIES = {'build': ['https://www.rdoproject.org/repos/rdo-release.rpm',
+                          'python34', 'python34-devel','python34-jinja2',
+                          'python34-markupsafe', 'python2-virtualbmc',
+                          'libguestfs-tools', 'bsdtar', 'libvirt',
+                          'python2-oslo-config', 'python2-debtcollector',
+                          'http://artifacts.opnfv.org/apex/dependencies'
+                          '/python3-ipmi-0.3.0-1.noarch.rpm',
+                          'Virtualization Host', 'python-tox']
+                }
+
+
+def check_package_dependencies(pkgs):
+    yb = yum.YumBase()
+    installed_pkgs = yb.rpmdb.returnPackages()
+    installed = [x.name for x in installed_pkgs]
+    for pkg in pkgs:
+        if pkg in installed:
+            print('{} is already installed'.format(pkg))
+        elif re.match('http', pkg) is not None:
+            print('Installing dependency package: {}'.format(pkg))
+            try:
+                subprocess.check_output(['yum', '-y', 'install', pkg])
+            except subprocess.CalledProcessError as e:
+                if 'does not update' not in e.output:
+                    print("Error installing package: {}".format(pkg))
+                    raise e
+        elif ' ' in pkg:
+            print('Installing group package: {}'.format(pkg))
+            subprocess.check_call(['yum', '-y', 'groupinstall', pkg])
+        else:
+            print('Installing dependency package: {}'.format(pkg))
+            pkg_def = {'name': pkg}
+            yb.install(**pkg_def)
+            yb.resolveDeps()
+            yb.buildTransaction()
+            yb.processTransaction()
+
+if __name__ == '__main__':
+    dep_parser = argparse.ArgumentParser()
+    dep_parser.add_argument('-d', '--dependency_type', required=True,
+                            dest='dep_type', help="Dependency type",
+                            choices=['build', 'deploy', 'clean'])
+    args = dep_parser.parse_args(sys.argv[1:])
+    check_package_dependencies(DEPENDENCIES[args.dep_type])
