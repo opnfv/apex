165d3cbceb673e2a0593cc71fbd97b4ec3e08706
Tim Rozet <trozet@redhat.com>
2017-06-25 21:25:36 -0400

    Migrates Apex to Python
    Removes all bash libraries and converts almost all of the code to a
    mixture of Python and Ansible.  utils.sh and clean.sh still exist.
    clean.sh will be migrated fully to clean.py in another patch.
    
    The Apex Python package is now built into the opnfv-apex-common RPM.  To
    install locally do 'pip3 install .'.  To deploy:
    
    opnfv-deploy -d <file> -n <file> --image-dir /root/apex/.build  -v --debug
    
    Non-python files (THT yaml, settings files, ansible playbooks) are all
    installed into /usr/share/opnfv-apex/.  The RPM will copy settings files
    into /etc/opnfv-apex/.
    
    JIRA: APEX-317
    
    Change-Id: I3232f0329bcd13bce5a28da6a8c9c84d0b048024
    Signed-off-by: Tim Rozet <trozet@redhat.com>

--- apex/tests/constants.py
+++ apex/tests/constants.py
@@ -1,6 +1,5 @@
-#!/bin/sh
 ##############################################################################
-# Copyright (c) 2016 Dan Radez (Red Hat) and others.
+# Copyright (c) 2017 Tim Rozet (trozet@redhat.com) and others.
 #
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Apache License, Version 2.0
@@ -8,9 +7,5 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-set -e
-rpm -q ansible || sudo yum -y install ansible
-ansible-playbook --become -i "localhost," -c local $DIR/../lib/ansible/playbooks/build_dependencies.yml -vvv
-make -C $DIR/../build clean
-python3 $DIR/../apex/build.py $@
+TEST_CONFIG_DIR = 'config'
+TEST_BUILD_DIR = 'build'
