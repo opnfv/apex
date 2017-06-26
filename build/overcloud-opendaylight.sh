b3c6dfb7049f1088c719269c045d6573569716aa
Feng Pan <fpan@redhat.com>
2017-06-26 12:02:46 -0400

    Pin puppet-opendaylight for puppet 3.8 compatibility
    Change-Id: Ib9052fafd601c7b269f0f6a3e98d83e63b07f12f
    Signed-off-by: Feng Pan <fpan@redhat.com>

--- build/overcloud-opendaylight.sh
+++ build/overcloud-opendaylight.sh
@@ -50,7 +50,7 @@ EOF
 rm -rf puppet-opendaylight
 git clone -b stable/carbon https://git.opendaylight.org/gerrit/integration/packaging/puppet-opendaylight
 pushd puppet-opendaylight > /dev/null
-git archive --format=tar.gz --prefix=opendaylight/ HEAD > ${BUILD_DIR}/puppet-opendaylight-carbon.tar.gz
+git archive --format=tar.gz --prefix=opendaylight/ If40937d33cd3a297edea25df8c4673b341236e97 > ${BUILD_DIR}/puppet-opendaylight-carbon.tar.gz
 git checkout stable/boron
 git archive --format=tar.gz --prefix=opendaylight/ HEAD > ${BUILD_DIR}/puppet-opendaylight-boron.tar.gz
 popd > /dev/null
