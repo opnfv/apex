2eb85cca9784be800dd80afc15e944b9fffa9733
Tim Rozet <trozet@redhat.com>
2017-06-25 21:25:36 -0400

    [WIP] Migrates Apex deploy to python
    Change-Id: I3232f0329bcd13bce5a28da6a8c9c84d0b048024
    Signed-off-by: Tim Rozet <trozet@redhat.com>

---
+++ lib/python/apex/virtual/vm_node.py
@@ -0,0 +1,17 @@
+##############################################################################
+# Copyright (c) 2017 Tim Rozet (trozet@redhat.com) and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+
+class VirtualNode:
+    """
+    VM object used by Apex
+    """
+
+    def __init__(self):
+        pass
