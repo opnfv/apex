323611a10eb3d21c858aac1bc2e1bb8e8e421757
Feng Pan <fpan@redhat.com>
2017-08-06 21:55:44 -0400

    Add os-odl-fdio-dvr support
    apex-tripleo-heat-templates: I6a83c9e2deeb0df9a3ab09a707c9c64aa84da55e
    apex-puppet-tripleo: I2025e3157b97b376b63002003ca17c7206aba546
    
    JIRA: APEX-466
    
    Change-Id: Iedb75994d35b5dc1dd5d5ce1a57277c8f3729dfd
    Signed-off-by: Feng Pan <fpan@redhat.com>

--- build/overcloud-opendaylight.sh
+++ build/overcloud-opendaylight.sh
@@ -49,9 +49,6 @@ popd > /dev/null
 # Download ODL netvirt for VPP
 populate_cache http://artifacts.opnfv.org/apex/danube/fdio_netvirt/opendaylight-7.0.0-0.1.20170531snap665.el7.noarch.rpm
 
-# Download ODL for fdio scenarios
-populate_cache http://artifacts.opnfv.org/apex/danube/fdio_odls/fdio_odl_carbon.tar.gz
-
 # install ODL packages
 # Patch in OPNFV custom puppet-tripleO
 # install Honeycomb
@@ -72,7 +69,6 @@ LIBGUESTFS_BACKEND=direct virt-customize \
     --install capnproto-libs,capnproto \
     --upload ${BUILD_ROOT}/patches/neutron-patch-NSDriver.patch:/usr/lib/python2.7/site-packages/ \
     --upload ${CACHE_DIR}/opendaylight-7.0.0-0.1.20170531snap665.el7.noarch.rpm:/root/ \
-    --upload ${CACHE_DIR}/fdio_odl_carbon.tar.gz:/root/ \
     -a overcloud-full-opendaylight_build.qcow2
 
 # Arch dependent on x86
@@ -84,8 +80,6 @@ populate_cache http://artifacts.opnfv.org/apex/danube/quagga/quagga-3.tar.gz
 populate_cache $honeycomb_pkg
 
 LIBGUESTFS_BACKEND=direct virt-customize \
-    --upload ${CACHE_DIR}/${honeycomb_pkg##*/}:/root/fdio/ \
-    --run-command "yum install -y /root/fdio/${honeycomb_pkg##*/}" \
     --install zeromq-4.1.4 \
     --upload ${CACHE_DIR}/quagga-3.tar.gz:/root/ \
     --run-command "cd /root/ && tar xzf quagga-3.tar.gz" \
