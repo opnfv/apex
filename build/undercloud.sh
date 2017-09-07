27e60ff7ecdf2c97960117e91f0f5cb6341e58e7
Tim Rozet <trozet@redhat.com>
2017-09-07 16:18:20 -0400

    Adds missing packages for undercloud
    JIRA: APEX-511
    
    Change-Id: If8ed2b5409e450c39f4fffd5a0b3cb96560c5d26
    Signed-off-by: Tim Rozet <trozet@redhat.com>

--- build/undercloud.sh
+++ build/undercloud.sh
@@ -38,6 +38,7 @@ LIBGUESTFS_BACKEND=direct virt-customize \
     --install "openstack-utils" \
     --install "ceph-common" \
     --install "python2-networking-sfc" \
+    --install openstack-ironic-inspector,subunit-filters,docker-distribution,openstck-tripleo-validations \
     --run-command "cd /usr/share && rm -rf openstack-tripleo-heat-templates && tar xzf apex-tripleo-heat-templates.tar.gz" \
     --run-command "sed -i '/ControllerEnableCephStorage/c\\  ControllerEnableCephStorage: true' /usr/share/openstack-tripleo-heat-templates/environments/storage-environment.yaml" \
     --run-command "sed -i '/ComputeEnableCephStorage/c\\  ComputeEnableCephStorage: true' /usr/share/openstack-tripleo-heat-templates/environments/storage-environment.yaml" \
