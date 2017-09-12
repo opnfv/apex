5ebc1ce9e9df3063f1c9a4a1f576f7bb6642b45d
Tim Rozet <trozet@redhat.com>
2017-09-12 17:32:56 -0400

    Adds ability to deploy from upstream openstack
    To deploy with upstream openstack branch, use new deploy setting 'os_version'.
    A default scenario file for nosdn with pike has been included in this patch.
    If 'os_version' is a version other than the default version for this OPNFV
    release, then upstream is used.
    
    In order to use upstream with the current OS version use '--upstream'
    argument to the deploy command, to force an upstream deployment.
    
    Supports nosdn and odl-nofeature deployments.
    
    Change-Id: Ic07e308827b449637b4e86cdd086434e4de2fb69
    Signed-off-by: Tim Rozet <trozet@redhat.com>

---
+++ apex/builders/common_builder.py
@@ -0,0 +1,111 @@
+##############################################################################
+# Copyright (c) 2017 Tim Rozet (trozet@redhat.com) and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+# Common building utilities for undercloud and overcloud
+
+import git
+import logging
+import os
+
+from apex import build_utils
+from apex.common import constants as con
+from apex.virtual import utils as virt_utils
+
+
+def project_to_path(project):
+    """
+    Translates project to absolute file path
+    :param project: name of project
+    :return: File path
+    """
+    if project.startswith('openstack/'):
+        project = os.path.basename(project)
+    if 'puppet' in project:
+        return "/etc/puppet/modules/{}".format(project.replace('puppet-', ''))
+    elif 'tripleo-heat-templates' in project:
+        return "/usr/share/openstack-tripleo-heat-templates"
+    else:
+        # assume python
+        return "/usr/lib/python2.7/site-packages/{}".format(project)
+
+
+def add_upstream_patches(patches, image, tmp_dir,
+                         default_branch=os.path.join('stable',
+                                                     con.DEFAULT_OS_VERSION)):
+    """
+    Adds patches from upstream OpenStack gerrit to Undercloud for deployment
+    :param patches: list of patches
+    :param image: undercloud image
+    :param tmp_dir: to store temporary patch files
+    :param default_branch: default branch to fetch commit (if not specified
+    in patch)
+    :return: None
+    """
+    virt_ops = list()
+    logging.debug("Evaluating upstream patches:\n{}".format(patches))
+    for patch in patches:
+        assert isinstance(patch, dict)
+        assert all(i in patch.keys() for i in ['project', 'change-id'])
+        if 'branch' in patch.keys():
+            branch = patch['branch']
+        else:
+            branch = default_branch
+        patch_diff = build_utils.get_patch(patch['change-id'],
+                                           patch['project'], branch)
+        if patch_diff:
+            patch_file = "{}.patch".format(patch['change-id'])
+            patch_file_path = os.path.join(tmp_dir, patch_file)
+            with open(patch_file_path, 'w') as fh:
+                fh.write(patch_diff)
+            project_path = project_to_path(patch['project'])
+            virt_ops.extend([
+                {con.VIRT_UPLOAD: "{}:{}".format(patch_file_path,
+                                                 project_path)},
+                {con.VIRT_RUN_CMD: "cd {} && patch -p1 < {}".format(
+                    project_path, patch_file)}])
+            logging.info("Adding patch {} to {}".format(patch_file,
+                                                        image))
+        else:
+            logging.info("Ignoring patch:\n{}".format(patch))
+    if virt_ops:
+        virt_utils.virt_customize(virt_ops, image)
+
+
+def add_repo(repo_url, repo_name, image, tmp_dir):
+    assert repo_name is not None
+    assert repo_url is not None
+    repo_file = "{}.repo".format(repo_name)
+    repo_file_path = os.path.join(tmp_dir, repo_file)
+    content = [
+        "[{}]".format(repo_name),
+        "name={}".format(repo_name),
+        "baseurl={}".format(repo_url),
+        "gpgcheck=0"
+    ]
+    logging.debug("Creating repo file {}".format(repo_name))
+    with open(repo_file_path, 'w') as fh:
+        fh.writelines(content)
+    logging.debug("Adding repo {} to {}".format(repo_file, image))
+    virt_utils.virt_customize([
+        {con.VIRT_UPLOAD: "{}:/etc/yum.repos.d/".format(repo_file_path)}],
+        image
+    )
+
+
+def create_git_archive(repo_url, repo_name, tmp_dir,
+                       branch='master', prefix=''):
+    repo = git.Repo.clone_from(repo_url, os.path.join(tmp_dir, repo_name))
+    repo_git = repo.git
+    if branch != str(repo.active_branch):
+        repo_git.checkout("origin/{}".format(branch))
+    archive_path = os.path.join(tmp_dir, "{}.tar".format(repo_name))
+    with open(archive_path, 'wb') as fh:
+        repo.archive(fh, prefix=prefix)
+    logging.debug("Wrote archive file: {}".format(archive_path))
+    return archive_path
---
+++ apex/network/network_data.py
@@ -0,0 +1,104 @@
+##############################################################################
+# Copyright (c) 2017 Tim Rozet (trozet@redhat.com) and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+import copy
+import logging
+import pprint
+
+from apex.common import utils
+from apex.common.constants import (
+    ADMIN_NETWORK,
+    TENANT_NETWORK,
+    STORAGE_NETWORK,
+    EXTERNAL_NETWORK,
+    API_NETWORK
+)
+from apex import NetworkSettings
+
+
+class NetworkDataException(Exception):
+    pass
+
+
+def create_network_data(ns, target=None):
+    """
+    Creates network data file for deployments
+    :param ns: Network Settings
+    :param target: Target file to write
+    :return: list of networks and properties
+    """
+    network_data = list()
+    if not isinstance(ns, NetworkSettings):
+        raise NetworkDataException('Invalid network settings given')
+
+    nets = ns['networks']
+
+    # TODO(trozet) change this all to be dynamic after TripleO bug
+    # https://bugs.launchpad.net/tripleo/+bug/1720849 is fixed
+
+    for net in nets.keys():
+        if net == ADMIN_NETWORK:
+            # we dont need to add ctlplane network to network data
+            continue
+        elif net == EXTERNAL_NETWORK:
+            network = nets[net][0]
+            net_name = net.title()
+            net_lower = net.lower()
+        elif net == API_NETWORK:
+            network = nets[net]
+            net_name = 'InternalApi'
+            net_lower = 'internal_api'
+        else:
+            network = nets[net]
+            net_name = net.title()
+            net_lower = net.lower()
+        # TODO(trozet): add ipv6 support
+        tmp_net = {'name': net_name,
+                   'name_lower': net_lower,
+                   'vip': net != TENANT_NETWORK,
+                   'enabled': net in ns.enabled_network_list}
+        if 'gateway' in network:
+            tmp_net['gateway_ip'] = str(network['gateway'])
+        if 'overcloud_ip_range' in network:
+            net_range = network['overcloud_ip_range']
+            tmp_net['allocation_pools'] = [{'start': str(net_range[0]),
+                                           'end': str(net_range[1])}]
+        elif tmp_net['enabled']:
+            logging.error("overcloud ip range is missing and must be provided "
+                          "in network settings when network is enabled for "
+                          "network {}".format(net))
+            raise NetworkDataException("overcloud_ip_range missing from "
+                                       "network: {}".format(net))
+        if 'cidr' in network:
+            tmp_net['ip_subnet'] = str(network['cidr'])
+        elif tmp_net['enabled']:
+            logging.error("cidr is missing and must be provided in network "
+                          "settings when network is enabled for network "
+                          "{}".format(net))
+            raise NetworkDataException("cidr is null for network {}".format(
+                net))
+
+        network_data.append(copy.deepcopy(tmp_net))
+
+    # have to do this due to the aforementioned bug
+    storage_mgmt_net = {
+        'name': 'StorageMgmt',
+        'enabled': False,
+        'name_lower': 'storage_mgmt',
+        'ip_subnet': '172.16.3.0/24',
+        'allocation_pools': [{'start': '172.16.3.4', 'end': '172.16.3.250'}],
+        'vip': True,
+    }
+    network_data.append(storage_mgmt_net)
+    if target:
+        logging.debug("Writing network data to {}".format(target))
+        utils.dump_yaml(network_data, target)
+    logging.debug("Network data parsed as:\n "
+                  "{}".format(pprint.pformat(network_data)))
+    return network_data
