c61e0f2ad6a1b290bb667e3bfd021ebaf12d8054
Tim Rozet <trozet@redhat.com>
2017-09-14 20:07:44 -0400

    Removes clean.sh
    CI has been migrated to use opnfv-clean for verify jobs, which is now
    installed as python.  There is no reason to keep ci/clean.sh anymore.
    
    Change-Id: I2edf23901c5debaba0674e3506fd0392d317dc6f
    Signed-off-by: Tim Rozet <trozet@redhat.com>

--- contrib/simple_deploy.sh
+++ contrib/simple_deploy.sh
@@ -3,9 +3,9 @@ set -e
 apex_home=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../
 export PYTHONPATH=$apex_home/apex:$PYTHONPATH
 $apex_home/ci/dev_dep_check.sh || true
-$apex_home/ci/clean.sh
 pip3 install -r $apex_home/requirements.txt
 pushd $apex_home/apex
+python3 clean.py
 echo "All further output will be piped to $PWD/nohup.out"
 (nohup python3 deploy.py -v -n ../config/network/network_settings.yaml -d ../config/deploy/os-nosdn-nofeature-noha.yaml --deploy-dir ../build --lib-dir ../lib --image-dir ../.build &)
 [ -f nohup.out ] || sleep 3
