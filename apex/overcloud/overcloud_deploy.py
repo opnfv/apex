131b89303760497fa4f3f8fe1a79afd4ac0d5821
Tim Rozet <trozet@redhat.com>
2017-09-28 15:00:17 -0400

    Fixes nova migration
    Nova migration was failing in rally sanity test.  Looking at the keys
    nova is configured with, the newlines in the private key were being
    converted to spaces in hieradata, thus making the key invalid.  This
    patch corrects the yaml so it is multiline data.
    
    Also, corrects the parameter being set to the right THT parameter and
    adds sshd service to the control/compute roles which is required for
    migration to work.
    
    JIRA: APEX-525
    
    Change-Id: Ic57db5c0b27df2e1f584f3c1a379e067291d892d
    Signed-off-by: Tim Rozet <trozet@redhat.com>

--- apex/overcloud/overcloud_deploy.py
+++ apex/overcloud/overcloud_deploy.py
@@ -345,9 +345,13 @@ def prep_env(ds, ns, opnfv_env, net_env, tmp_dir):
         if 'CloudDomain' in line:
             output_line = "  CloudDomain: {}".format(ns['domain_name'])
         elif 'replace_private_key' in line:
-            output_line = "      key: '{}'".format(private_key)
+            output_line = "    private_key: |\n"
+            key_out = ''
+            for line in private_key.splitlines():
+                key_out += "      {}\n".format(line)
+            output_line += key_out
         elif 'replace_public_key' in line:
-            output_line = "      key: '{}'".format(public_key)
+            output_line = "    public_key: '{}'".format(public_key)
 
         if ds_opts['sdn_controller'] == 'opendaylight' and \
                 'odl_vpp_routing_node' in ds_opts and ds_opts[
