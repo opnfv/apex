a4747b592ee4ad35676ac2740fe60bd13ba48a0d
Dan Radez <dradez@redhat.com>
2017-10-20 15:08:49 -0400

    Make introspection optional
    - exposes new option to end users to skip introspection
    - moves the logic to decide to introspect or not into python
    
    JIRA: APEX-536
    
    Change-Id: Ieaff11362ff8f906daa98d301d3d473ad549d08f
    Signed-off-by: Dan Radez <dradez@redhat.com>

--- apex/deploy.py
+++ apex/deploy.py
@@ -237,7 +237,11 @@ def main():
     net_env = NetworkEnvironment(net_settings, net_env_file)
     net_env_target = os.path.join(APEX_TEMP_DIR, constants.NET_ENV_FILE)
     utils.dump_yaml(dict(net_env), net_env_target)
+
+    # get global deploy params
     ha_enabled = deploy_settings['global_params']['ha_enabled']
+    introspect = deploy_settings['global_params'].get('introspect', True)
+
     if args.virtual:
         if args.virt_compute_ram is None:
             compute_ram = args.virt_default_ram
@@ -363,6 +367,9 @@ def main():
         deploy_vars['virtual'] = args.virtual
         deploy_vars['debug'] = args.debug
         deploy_vars['aarch64'] = platform.machine() == 'aarch64'
+        deploy_vars['introspect'] = not (args.virtual or
+                                         deploy_vars['aarch64'] or
+                                         not introspect)
         deploy_vars['dns_server_args'] = ''
         deploy_vars['apex_temp_dir'] = APEX_TEMP_DIR
         deploy_vars['stackrc'] = 'source /home/stack/stackrc'
