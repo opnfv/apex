517be618410806d5a169c04b665b11b0f033d38a
Tim Rozet <trozet@redhat.com>
2017-12-04 11:20:23 -0500

    Enables containerized overcloud deployments
    Changes Include:
      - For upstream deployments, Docker local registry will be updated with
        latest current RDO containers, regular deployments will use latest
        stable
      - Upstream container images will then be patched/modified and then
        re-uploaded into local docker registry
      - Deployment command modified to deploy with containers
    
    JIRA: APEX-549
    
    Change-Id: I0652c194c059b915a942ac7401936e8f5c69d1fa
    Signed-off-by: Tim Rozet <trozet@redhat.com>

--- apex/builders/common_builder.py
+++ apex/builders/common_builder.py
@@ -13,8 +13,10 @@ import git
 import logging
 import os
 
+import apex.builders.overcloud_builder as oc_builder
 from apex import build_utils
 from apex.common import constants as con
+from apex.common import utils
 from apex.virtual import utils as virt_utils
 
 
@@ -35,9 +37,28 @@ def project_to_path(project):
         return "/usr/lib/python2.7/site-packages/{}".format(project)
 
 
+def project_to_docker_service(project, os_version):
+    """
+    Translates OpenStack project to OOO services that are containerized
+    :param project: name of OpenStack project
+    :param os_version: version of OpenStack to check
+    :return: List of OOO service names
+    """
+    url = "https://raw.githubusercontent.com/openstack/tripleo-heat" \
+          "-templates/{}/environments/docker.yaml".format(os_version)
+    all_docker_services = utils.parse_yaml(utils.open_webpage(url))
+    project_services = list()
+    for service in all_docker_services.keys():
+        if project in service.lower():
+            project_services.append(service.replace(
+                'OS::TripleO::Services::', ''))
+    return project_services
+
+
 def add_upstream_patches(patches, image, tmp_dir,
                          default_branch=os.path.join('stable',
-                                                     con.DEFAULT_OS_VERSION)):
+                                                     con.DEFAULT_OS_VERSION),
+                         uc_ip=None, os_version=None, docker_tag=None):
     """
     Adds patches from upstream OpenStack gerrit to Undercloud for deployment
     :param patches: list of patches
@@ -45,10 +66,14 @@ def add_upstream_patches(patches, image, tmp_dir,
     :param tmp_dir: to store temporary patch files
     :param default_branch: default branch to fetch commit (if not specified
     in patch)
-    :return: None
+    :param uc_ip: undercloud IP (required only for docker patches)
+    :param os_version: OpenStack version (required only for docker patches)
+    :param docker_tag: Docker Tag (required only for docker patches)
+    :return: Set of docker services patched (if applicable)
     """
     virt_ops = [{con.VIRT_INSTALL: 'patch'}]
     logging.debug("Evaluating upstream patches:\n{}".format(patches))
+    docker_services = set()
     for patch in patches:
         assert isinstance(patch, dict)
         assert all(i in patch.keys() for i in ['project', 'change-id'])
@@ -60,21 +85,49 @@ def add_upstream_patches(patches, image, tmp_dir,
                                            patch['project'], branch)
         if patch_diff:
             patch_file = "{}.patch".format(patch['change-id'])
-            patch_file_path = os.path.join(tmp_dir, patch_file)
+            project_path = project_to_path(patch['project'])
+            # If docker tag and python we know this patch belongs on docker
+            # container for a docker service. Therefore we build the dockerfile
+            # and move the patch into the containers directory.  We also assume
+            # this builder call is for overcloud, because we do not support
+            # undercloud containers
+            if docker_tag and 'python' in project_path:
+                # Projects map to multiple THT services, need to check which
+                # are supported
+                ooo_docker_services = project_to_docker_service(
+                    patch['project'], os_version)
+            else:
+                ooo_docker_services = []
+            # If we found services, then we treat the patch like it applies to
+            # docker only
+            if ooo_docker_services:
+                for service in ooo_docker_services:
+                    docker_services = docker_services.union(service)
+                    docker_cmds = [
+                        "WORKDIR {}".format(project_path),
+                        "ADD {} {}".format(patch_file, project_path),
+                        "RUN patch -p1 < {}".format(patch_file)
+                    ]
+                    oc_builder.build_dockerfile(service, tmp_dir, docker_cmds,
+                                                uc_ip, docker_tag, os_version)
+                patch_file_path = os.path.join(tmp_dir, 'containers',
+                                               patch_file)
+            else:
+                patch_file_path = os.path.join(tmp_dir, patch_file)
+                virt_ops.extend([
+                    {con.VIRT_UPLOAD: "{}:{}".format(patch_file_path,
+                                                     project_path)},
+                    {con.VIRT_RUN_CMD: "cd {} && patch -p1 < {}".format(
+                        project_path, patch_file)}])
+                logging.info("Adding patch {} to {}".format(patch_file,
+                                                            image))
             with open(patch_file_path, 'w') as fh:
                 fh.write(patch_diff)
-            project_path = project_to_path(patch['project'])
-            virt_ops.extend([
-                {con.VIRT_UPLOAD: "{}:{}".format(patch_file_path,
-                                                 project_path)},
-                {con.VIRT_RUN_CMD: "cd {} && patch -p1 < {}".format(
-                    project_path, patch_file)}])
-            logging.info("Adding patch {} to {}".format(patch_file,
-                                                        image))
         else:
             logging.info("Ignoring patch:\n{}".format(patch))
     if len(virt_ops) > 1:
         virt_utils.virt_customize(virt_ops, image)
+    return docker_services
 
 
 def add_repo(repo_url, repo_name, image, tmp_dir):
