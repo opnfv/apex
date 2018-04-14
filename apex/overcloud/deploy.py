e19054ab46286ec4d28d935f3524f2035bec2e80
Feng Pan <fpan@redhat.com>
2018-04-13 22:22:40 -0400

    Fix nosdn fdio scenario
    - Update networking-vpp to use master branch
    - Update THT vpp ml2 environment file with correct parameters
    - Update external network creation command to use 'externa' as
      provider network name.
    - Remove vpp network settings file as it's not used
    
    apex-tripleo-heat-templates: Ia25db8456f1ad6beb96c7b9b5f318b166ef4576a
    apex-puppet-tripleo: I231054a433eb7e598a6e24f6eaea02d476e776de
    
    Change-Id: I4a1f68c75ae3b7d2a5b347d05abf0d025e8b116b
    Signed-off-by: Feng Pan <fpan@redhat.com>

--- apex/overcloud/deploy.py
+++ apex/overcloud/deploy.py
@@ -555,8 +555,9 @@ def prep_env(ds, ns, inv, opnfv_env, net_env, tmp_dir):
                                        ns['domain_name']))
         elif not ds_opts['sdn_controller'] and ds_opts['dataplane'] == 'fdio':
             if 'NeutronVPPAgentPhysnets' in line:
-                output_line = ("  NeutronVPPAgentPhysnets: 'datacentre:{}'".
-                               format(tenant_nic['Controller']))
+                output_line = ("  NeutronVPPAgentPhysnets: " \
+                               "'datacentre:{},external:tap0'"
+                               .format(tenant_nic['Controller']))
         elif ds_opts['sdn_controller'] == 'opendaylight' and ds_opts.get(
                 'dvr') is True:
             if 'OS::TripleO::Services::NeutronDhcpAgent' in line:
