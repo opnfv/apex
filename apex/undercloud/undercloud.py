0a0058ec490ad73892c606752dc53ef28b72f026
Ricardo Noriega <rnoriega@redhat.com>
2018-08-07 18:29:54 +0200

    [WIP] Enable SFC scenarios for Gambia
      - This patch will install OVS 2.9.2 including
        its kernel module which allows native NSH
        headers.
      - Fix Custom OVS due to bug:
        https://bugzilla.redhat.com/show_bug.cgi?id=1544892
    
    JIRA: APEX-630
    
    Change-Id: Ia410309fd7053602ce78eae919839d0f57c9742a
    Signed-off-by: Ricardo Noriega <rnoriega@redhat.com>

--- apex/undercloud/undercloud.py
+++ apex/undercloud/undercloud.py
@@ -222,6 +222,9 @@ class Undercloud:
         config = dict()
         # Check if this is an ARM deployment
         config['aarch64'] = platform.machine() == 'aarch64'
+        logging.info("RICKY: ds is {}".format(ds))
+        if 'sfc' in ds['deploy_options']:
+            config['sfc'] = ds['deploy_options']['sfc']
         # Configuration for undercloud.conf
         config['undercloud_config'] = [
             "enable_ui false",
