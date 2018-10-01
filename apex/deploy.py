fcea98140dfb5d5bbe1d8b46c5b96e44df0c4310
Charalampos Kominos <Charalampos.Kominos@enea.com>
2018-10-01 13:12:46 +0200

    WIP: Bring in aarch64 support in apex
    RDO builds packages which are aarch64 compatible but some configuration
    is needed to succesfully deploy.
    This change:
    
    - Prepares the aarch64 container repo as the source for Kolla Containers
    - Configures VM sizing for aarch64 undercloud.
    - Configures VM sizing for aarch64 virtual deploy targets.
      Vms need to be larger on aarch64 compared to x86 to avoid
      starvation of resources. (MYSQL)
    - Configures vda2 as the location of the Linux Kernel in aarch64 in
      an UEFI system
    - Configures the vNICs to be on the pci-bus instead of the virtio-mmio
      bus.This will enalbe the Nics to come up in the same order as the
      x86 ones,  so the extra configuration in ansible is not needed
    - Enables the Keepalived Container which is not properly pulled on apex
      BUG: APEX-640
    - Configures apex to use a newer version of the ceph:daemon container
    
    JIRA: APEX-619
    
    Change-Id: Ia3f067821e12bba44939bbf8c0e4676f2da70239
    Signed-off-by: Charalampos Kominos <Charalampos.Kominos@enea.com>
    Signed-off-by: ting wu <ting.wu@enea.com>

--- apex/deploy.py
+++ apex/deploy.py
@@ -239,7 +239,9 @@ def main():
     # when CI is changed
     if os.getenv('IMAGES', False):
         args.image_dir = os.getenv('IMAGES')
-    if args.debug:
+    if args.debug:./apex/overcloud/deploy.py:256:10: E111 indentation is not a multiple of four
+./apex/virtual/utils.py:70:8: E111 indentation is not a multiple of four
+
         log_level = logging.DEBUG
     else:
         log_level = logging.INFO
@@ -282,15 +284,23 @@ def main():
                 compute_ram = args.virt_compute_ram
             if (deploy_settings['deploy_options']['sdn_controller'] ==
                     'opendaylight' and args.virt_default_ram < 12):
-                control_ram = 12
+                if platform.machine() == 'aarch64':
+                    control_ram = 16
+                else:
+                    control_ram = 12
                 logging.warning('RAM per controller is too low.  OpenDaylight '
                                 'requires at least 12GB per controller.')
                 logging.info('Increasing RAM per controller to 12GB')
             elif args.virt_default_ram < 10:
-                control_ram = 10
-                logging.warning('RAM per controller is too low.  nosdn '
-                                'requires at least 10GB per controller.')
-                logging.info('Increasing RAM per controller to 10GB')
+                if platform.machine() == 'aarch64':
+                    control_ram = 16
+                    logging.warning('aarch64 requires at least 16GB per '
+                                    'controller. Increasing to 16.')
+                else:
+                    control_ram = 10
+                    logging.warning('RAM per controller is too low.  nosdn '
+                                    'requires at least 10GB per controller.')
+                    logging.info('Increasing RAM per controller to 10GB')
             else:
                 control_ram = args.virt_default_ram
             if ha_enabled and args.virt_compute_nodes < 2:
