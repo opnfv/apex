737708b6e4a3dd6d897c2d8af3459bab060d30c2
Tim Rozet <trozet@redhat.com>
2018-10-24 12:25:20 -0400

    Change OPNFV Artifacts URL
    This change allows downloading of files larger than 20GB.
    
    Change-Id: I881ea35316e67980d4144e46bfbd41501e80f497
    Signed-off-by: Tim Rozet <trozet@redhat.com>
    (cherry picked from commit 498356c9efb765a5714845ce8303dc8a264eb300)

--- apex/common/constants.py
+++ apex/common/constants.py
@@ -70,7 +70,7 @@ VALID_DOCKER_SERVICES = {
 DOCKERHUB_OOO = 'https://registry.hub.docker.com/v2/repositories' \
                 '/tripleomaster/'
 KUBESPRAY_URL = 'https://github.com/kubernetes-incubator/kubespray.git'
-OPNFV_ARTIFACTS = 'http://artifacts.opnfv.org'
+OPNFV_ARTIFACTS = 'http://storage.googleapis.com/artifacts.opnfv.org'
 CUSTOM_OVS = '{}/apex/random/openvswitch-2.9.0-9.el7fdn.x86_64.' \
              'rpm'.format(OPNFV_ARTIFACTS)
 
