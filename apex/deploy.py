4bfdcd43851168cc3fc152c4607ea497926f2afe
Tim Rozet <trozet@redhat.com>
2018-11-05 09:30:32 -0500

    Remove downloading undercloud.qcow2
    OOO team is removing the undercloud disk image as it is no longer needed
    for containerized undercloud deployments. Instead, we can just use the
    overcloud image as the undercloud image.
    
    Additionally, OOO team has recommended we use current-tripleo instead of
    current-tripleo-rdo. current-tripleo-rdo was previously thought to be
    more stable with more promotion checks, but now it seems that it is
    older and current-tripleo now has the same stability/checks.
    
    Change-Id: I9e6bb2260dbe9f8796ee54d20527c0aad96476ec
    Signed-off-by: Tim Rozet <trozet@redhat.com>

--- apex/deploy.py
+++ apex/deploy.py
@@ -377,16 +377,18 @@ def main():
         args.image_dir = os.path.join(args.image_dir, os_version)
         upstream_url = constants.UPSTREAM_RDO.replace(
             constants.DEFAULT_OS_VERSION, os_version)
-        upstream_targets = ['overcloud-full.tar', 'undercloud.qcow2']
+        upstream_targets = ['overcloud-full.tar']
         utils.fetch_upstream_and_unpack(args.image_dir, upstream_url,
                                         upstream_targets,
                                         fetch=not args.no_fetch)
         sdn_image = os.path.join(args.image_dir, 'overcloud-full.qcow2')
         # copy undercloud so we don't taint upstream fetch
         uc_image = os.path.join(args.image_dir, 'undercloud_mod.qcow2')
-        uc_fetch_img = os.path.join(args.image_dir, 'undercloud.qcow2')
+        uc_fetch_img = sdn_image
         shutil.copyfile(uc_fetch_img, uc_image)
         # prep undercloud with required packages
+        uc_builder.update_repos(image=uc_image,
+                                branch=branch.replace('stable/', ''))
         uc_builder.add_upstream_packages(uc_image)
         uc_builder.inject_calipso_installer(APEX_TEMP_DIR, uc_image)
         # add patches from upstream to undercloud and overcloud
